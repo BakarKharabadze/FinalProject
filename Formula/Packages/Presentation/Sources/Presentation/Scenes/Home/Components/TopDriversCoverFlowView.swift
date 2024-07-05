a18aaf8005ba401895b8124a39d43e531c01dd76
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-07-05 21:42:50 +0400

    Add TopDriversCoverFlow

---
+++ Formula/Packages/Presentation/Sources/Presentation/Scenes/Home/Components/TopDriversCoverFlowView.swift
@@ -0,0 +1,42 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Bakar Kharabadze on 7/4/24.
+//
+
+import SwiftUI
+import Domain
+
+struct TopDriversCoverFlowView: View {
+    @State private var spacing: CGFloat = 0
+    @State private var rotation: CGFloat = 65
+    @State private var enableReflection: Bool = false
+    
+    let drivers: [DriverEntity]
+    
+    var body: some View {
+        CoverFlowView(
+            itemWidth: 280,
+            enableReflection: enableReflection,
+            spacing: spacing,
+            rotation: rotation,
+            items: drivers
+        ) { driver in
+            HStack {
+                Text(driver.name)
+                    .font(.title)
+                VStack {
+                    Image(driver.imageName, bundle: .module)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 150)
+                }
+            }
+            .frame(width: 340, height: 150)
+            .background(Color.gray)
+            .cornerRadius(10)
+        }
+        .frame(height: 180)
+    }
+}
