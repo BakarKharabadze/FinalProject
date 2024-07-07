8a40dc6bc12934069a7015cafaafde3e306c75aa
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-07-07 22:27:52 +0400

    Add Drivers Table view Cell

---
+++ Formula/Packages/Presentation/Sources/Presentation/Scenes/Standing/Components/Driver.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bakar Kharabadze on 7/7/24.
+//
+
+import Foundation
+
+struct Driver {
+    let ranking: String
+    let name: String
+    let team: String
+    let points: Int
+    let imageName: String
+}
