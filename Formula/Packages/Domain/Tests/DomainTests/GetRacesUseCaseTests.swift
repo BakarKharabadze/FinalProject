83fd64e9ff3ab616ed1ba00e89e2b0a1d3aab340
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-07-19 02:25:13 +0400

    Add GetRacesUseCaseTests

---
+++ Formula/Packages/Domain/Tests/DomainTests/GetRacesUseCaseTests.swift
@@ -0,0 +1,97 @@
+//
+//  File.swift
+//
+//
+//  Created by Bakar Kharabadze on 7/19/24.
+//
+
+import Common
+import XCTest
+@testable import Domain
+
+class GetRacesUseCaseTests: XCTestCase {
+    
+    var useCase: DefaultGetRacesUseCase!
+    var repository: MockRaceRepository!
+    
+    override func setUp() {
+        super.setUp()
+        repository = MockRaceRepository()
+        useCase = DefaultGetRacesUseCase(repository: repository)
+    }
+    
+    override func tearDown() {
+        repository = nil
+        useCase = nil
+        super.tearDown()
+    }
+    
+    func testExecuteSuccess() {
+        // Given
+        let expectedRaces = [RaceEntity(
+            round: "",
+            grandPrixName: "",
+            date: "",
+            image: "",
+            startsIn: "",
+            countryFlag: "",
+            circuitID: "")]
+        repository.result = .success(expectedRaces)
+        
+        // When
+        let expectation = self.expectation(description: "Completion handler called")
+        var receivedResult: Result<[RaceEntity], Error>?
+        _ = useCase.execute { response in
+            receivedResult = response
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        
+        // Then
+        switch receivedResult {
+        case .success(let races)?:
+            XCTAssertEqual(races, expectedRaces)
+        case .failure, .none:
+            XCTFail("Expected success, but got failure or nil")
+        }
+    }
+    
+    func testExecuteFailure() {
+        // Given
+        let expectedError = NSError(domain: "TestError", code: 0, userInfo: nil)
+        repository.result = .failure(expectedError)
+        
+        // When
+        let expectation = self.expectation(description: "Completion handler called")
+        var receivedResult: Result<[RaceEntity], Error>?
+        _ = useCase.execute { response in
+            receivedResult = response
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        
+        // Then
+        switch receivedResult {
+        case .success:
+            XCTFail("Expected failure, but got success")
+        case .failure(let error as NSError)?:
+            XCTAssertEqual(error, expectedError)
+        case .none:
+            XCTFail("Expected failure, but got nil")
+        }
+    }
+}
+
+class MockRaceRepository: RaceRepository {
+    
+    var result: Result<[RaceEntity], Error>?
+    
+    func getRaces(result: @escaping (Result<[RaceEntity], Error>) -> Void) -> Cancellable? {
+        if let resultValue = self.result {
+            result(resultValue)
+        }
+        return nil
+    }
+}
