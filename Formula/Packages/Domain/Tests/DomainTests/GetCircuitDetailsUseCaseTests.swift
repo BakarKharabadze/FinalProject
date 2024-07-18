c4ce64c692bb0a29e73542cef805a5f1a32554eb
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-07-19 01:56:12 +0400

    Add GetCircuitDetailsUseCaseTests

---
+++ Formula/Packages/Domain/Tests/DomainTests/GetCircuitDetailsUseCaseTests.swift
@@ -0,0 +1,108 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bakar Kharabadze on 7/19/24.
+//
+
+import XCTest
+import Common
+@testable import Domain
+
+class GetCircuitDetailsUseCaseTests: XCTestCase {
+    
+    var useCase: DefaultGetCircuitDetailsUseCase!
+    var repository: MockFormulaSportsRepository!
+    
+    override func setUp() {
+        super.setUp()
+        repository = MockFormulaSportsRepository()
+        useCase = DefaultGetCircuitDetailsUseCase(repository: repository)
+    }
+    
+    override func tearDown() {
+        repository = nil
+        useCase = nil
+        super.tearDown()
+    }
+    
+    func testExecuteSuccess() {
+        // Given
+        let expectedDetails = [CircuitDetailsEntity(
+            countryName: "",
+            grandPrixName: "",
+            date: "",
+            circuitLenght: "",
+            laps: "",
+            turns: "",
+            topSpeed: "",
+            practiceOneDate: "",
+            practiceOneTime: "",
+            practiceTwoDate: "",
+            practiceTwoTime: "",
+            practiceThreeDate: "",
+            practiceThreeTime: "",
+            qualificationDate: "",
+            qualificationTime: "",
+            raceDate: "",
+            raceTime: ""
+        )]
+        repository.result = .success(expectedDetails)
+        
+        // When
+        let expectation = self.expectation(description: "Completion handler called")
+        var result: Result<[CircuitDetailsEntity], Error>?
+        _ = useCase.execute(for: "Circuit1") { response in
+            result = response
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        
+        // Then
+        switch result {
+        case .success(let details)?:
+            XCTAssertEqual(details, expectedDetails)
+        case .failure, .none:
+            XCTFail("Expected success, but got failure or nil")
+        }
+    }
+    
+    func testExecuteFailure() {
+        // Given
+        let expectedError = NSError(domain: "TestError", code: 0, userInfo: nil)
+        repository.result = .failure(expectedError)
+        
+        // When
+        let expectation = self.expectation(description: "Completion handler called")
+        var result: Result<[CircuitDetailsEntity], Error>?
+        _ = useCase.execute(for: "Circuit1") { response in
+            result = response
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        
+        // Then
+        switch result {
+        case .success:
+            XCTFail("Expected failure, but got success")
+        case .failure(let error as NSError)?:
+            XCTAssertEqual(error, expectedError)
+        case .none:
+            XCTFail("Expected failure, but got nil")
+        }
+    }
+}
+
+class MockFormulaSportsRepository: FormulaSportsRepository {
+    
+    var result: Result<[CircuitDetailsEntity], Error>?
+    
+    func getCircuitDetails(for circuitName: String, completion: @escaping (Result<[CircuitDetailsEntity], Error>) -> Void) -> Cancellable? {
+        if let result = result {
+            completion(result)
+        }
+        return nil // Or return a mock Cancellable if needed
+    }
+}
