39a08c35fb7d3ce824478924574a3a78d57ac618
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-07-19 02:13:17 +0400

    Add GetTeamDetailsUseCaseTests

---
+++ Formula/Packages/Domain/Tests/DomainTests/GetTeamDetailsUseCaseTests.swift
@@ -0,0 +1,100 @@
+//
+//  GetTeamDetailsUseCaseTests.swift
+//
+//
+//  Created by Bakar Kharabadze on 7/19/24.
+//
+
+import Common
+import XCTest
+@testable import Domain
+
+class GetTeamDetailsUseCaseTests: XCTestCase {
+    
+    var useCase: DefaultGetTeamDetailsUseCase!
+    var repository: MockTeamDetailsRepository!
+    
+    override func setUp() {
+        super.setUp()
+        repository = MockTeamDetailsRepository()
+        useCase = DefaultGetTeamDetailsUseCase(repository: repository)
+    }
+    
+    override func tearDown() {
+        repository = nil
+        useCase = nil
+        super.tearDown()
+    }
+    
+    func testExecuteSuccess() {
+        // Given
+        let expectedDetails = [TeamDetailsEntity(
+            name: "",
+            worldTitle: "",
+            raceWins: "",
+            polePositions: "",
+            fastesLaps: "",
+            base: "",
+            president: "",
+            director: "",
+            technicalManeger: "")]
+        repository.result = .success(expectedDetails)
+        
+        // When
+        let expectation = self.expectation(description: "Completion handler called")
+        var result: Result<[TeamDetailsEntity], Error>?
+        _ = useCase.execute { response in
+            result = response
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        
+        // Then
+        switch result {
+        case .success(let details)?:
+            XCTAssertEqual(details, expectedDetails)
+        case .failure, .none:
+            XCTFail("Expected success, but got failure or nil")
+        }
+    }
+    
+    func testExecuteFailure() {
+        // Given
+        let expectedError = NSError(domain: "TestError", code: 0, userInfo: nil)
+        repository.result = .failure(expectedError)
+        
+        // When
+        let expectation = self.expectation(description: "Completion handler called")
+        var result: Result<[TeamDetailsEntity], Error>?
+        _ = useCase.execute { response in
+            result = response
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        
+        // Then
+        switch result {
+        case .success:
+            XCTFail("Expected failure, but got success")
+        case .failure(let error as NSError)?:
+            XCTAssertEqual(error, expectedError)
+        case .none:
+            XCTFail("Expected failure, but got nil")
+        }
+    }
+}
+
+class MockTeamDetailsRepository: TeamDetailsRepository {
+    
+    var result: Result<[TeamDetailsEntity], Error>?
+    
+    func getTeamDetails(completion: @escaping (Result<[TeamDetailsEntity], Error>) -> Void) -> Cancellable? {
+        if let result = result {
+            completion(result)
+        }
+        return nil 
+    }
+}
+
