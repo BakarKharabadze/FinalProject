634a7f20b40d0a2059526e478cd70d80a997da68
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-07-24 21:55:44 +0400

    Add TeamsViewController Loader

--- Formula/Packages/Presentation/Sources/Presentation/Scenes/TeamsStanding/TeamsViewController.swift
+++ Formula/Packages/Presentation/Sources/Presentation/Scenes/TeamsStanding/TeamsViewController.swift
@@ -6,18 +6,22 @@
 //
 
 import UIKit
+import Domain
 
 public final class TeamsViewController: UIViewController {
     
     // MARK: - Properties
     private let tableView = UITableView()
+    private let activityIndicator = UIActivityIndicatorView(style: .large)
     private var viewModel: TeamsViewModel!
+    private let errorMessageLabel = UILabel()
 
     // MARK: - Initialization
     public class func create(with viewModel: TeamsViewModel) -> TeamsViewController {
         let vc = TeamsViewController()
         vc.viewModel = viewModel
         vc.viewModel.router = vc
+        vc.viewModel.delegate = vc
         return vc
     }
     
@@ -26,28 +30,32 @@ public final class TeamsViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "CustomBackground")
         navigationController?.navigationBar.barTintColor = UIColor(named: "CustomBackground")
-        setupTableView()
-        setupTitleLabel()
+        setupUI()
+        viewModel.fetchTeams()
     }
     
-    // MARK: - Setup Functions
+    // MARK: - UI Setup
+    private func setupUI() {
+        setupTableView()
+        setupTitleLabel()
+        setupActivityIndicator()
+        setupErrorMessageLabel()
+    }
+    
     private func setupTableView() {
-        tableView.register(TeamsTableViewCell.self, forCellReuseIdentifier: "TeamsTableViewCell")
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.dataSource = self
         tableView.delegate = self
         tableView.backgroundColor = UIColor(named: "CustomBackground")
-        
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(TeamsTableViewCell.self, forCellReuseIdentifier: "TeamsTableViewCell")
         
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-        
-        tableView.reloadData()
     }
     
     private func setupTitleLabel() {
@@ -58,13 +66,60 @@ public final class TeamsViewController: UIViewController {
             NSAttributedString.Key.font: font
         ]
     }
-
+    
+    private func setupActivityIndicator() {
+        activityIndicator.color = .white
+        activityIndicator.hidesWhenStopped = true
+        view.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func setupErrorMessageLabel() {
+        errorMessageLabel.textColor = .white
+        errorMessageLabel.font = .systemFont(ofSize: 22, weight: .bold)
+        errorMessageLabel.numberOfLines = 0
+        errorMessageLabel.textAlignment = .center
+        errorMessageLabel.isHidden = true
+        view.addSubview(errorMessageLabel)
+        errorMessageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            errorMessageLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            errorMessageLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            errorMessageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            errorMessageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    // MARK: - Loader Functions
+    private func showLoader() {
+        activityIndicator.startAnimating()
+        tableView.isHidden = true
+        errorMessageLabel.isHidden = true
+    }
+    
+    private func hideLoader() {
+        activityIndicator.stopAnimating()
+        tableView.isHidden = false
+    }
+    
+    public func showErrorText(message: String) {
+        errorMessageLabel.text = message
+        errorMessageLabel.isHidden = false
+        tableView.isHidden = true
+        activityIndicator.stopAnimating()
+    }
 }
 
-// MARK: - UITableViewDataSource, UITableViewDelegate
-extension TeamsViewController: UITableViewDataSource, UITableViewDelegate {
+// MARK: - UITableViewDataSource
+extension TeamsViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        viewModel.teams.count
+        return viewModel.teams.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -73,7 +128,10 @@ extension TeamsViewController: UITableViewDataSource, UITableViewDelegate {
         cell.configure(with: team, viewModel: viewModel)
         return cell
     }
-    
+}
+
+// MARK: - UITableViewDelegate
+extension TeamsViewController: UITableViewDelegate {
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let team = viewModel.teams[indexPath.row]
         viewModel.teamViewTapped(team: team)
@@ -88,5 +146,27 @@ extension TeamsViewController: TeamsViewRouter {
             let teamDetailsVC = TeamDetailsViewController.create(with: viewModel)
             navigationController?.pushViewController(teamDetailsVC, animated: true)
         }
+    }
+}
+
+// MARK: - TeamsViewModelDelegate
+extension TeamsViewController: TeamsViewModelDelegate {
+    public func teamsFetched(_ teams: [TeamsEntity]) {
+        DispatchQueue.main.async {
+            self.hideLoader()
+            self.tableView.reloadData()
+        }
+    }
+    
+    public func showLoading() {
+        showLoader()
+    }
+    
+    public func hideLoading() {
+        hideLoader()
+    }
+    
+    public func showError(message: String) {
+        showErrorText(message: message)
     }
 }
